074d8ec702f8c8d907298c559ba0b862fb2821ec
nhsltn <nhsltndev@gmail.com>
2026-04-07 14:26:01 +0700

    feat: implement interceptor

--- src/services/api/user.js
+++ src/services/api/user.js
@@ -4,6 +4,14 @@ const userApi = axios.create({
   baseURL: import.meta.env.VITE_MOCKAPI_BASE_URL,
 });
 
+userApi.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("User API Error:", error.response?.data || error.message);
+    return Promise.reject(error);
+  },
+);
+
 export const fetchAllUsers = () => userApi.get("/users");
 
 export const fetchUser = (userId) => userApi.get(`/users/${userId}`);
